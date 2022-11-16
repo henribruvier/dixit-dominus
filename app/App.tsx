ba933e45cb070ba63d9e2639c1a04b64e10ddf7c
henribruvier <henri.bruvier@gmail.com>
2022-11-16 11:56:29 +0100

    fix: :bug: fix install with pnpm

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,28 @@
 import {StatusBar} from 'expo-status-bar';
 import {Text, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {ThemeProvider, createTheme, Button} from '@rneui/themed';
+
+const theme = createTheme({
+	lightColors: {
+		primary: '#e7e7e8',
+	},
+	darkColors: {
+		primary: '#000',
+	},
+	mode: 'light',
+});
 
 export default function App() {
 	return (
-		<View className='flex-1 items-center justify-center bg-white'>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<SafeAreaProvider>
+			<ThemeProvider theme={theme}>
+				<View className='flex-1 items-center justify-center bg-white'>
+					<Text>Open up App.tsx to start working on your app!</Text>
+					<StatusBar style='auto' />
+					<Button className='bg-red-300'>Button</Button>
+				</View>
+			</ThemeProvider>
+		</SafeAreaProvider>
 	);
 }
