0d629856fbda3d4d907b694b6e4bf54083c70518
henribruvier <henri.bruvier@gmail.com>
2022-11-16 15:15:39 +0100

    feat: :construction: work in progress in app

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,19 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {ThemeProvider, createTheme, Button} from '@rneui/themed';
 import {NavigationContainer} from '@react-navigation/native';
 import MyStack from './src/routes/stack';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import {Subscription} from 'expo-modules-core';
+import {useEffect, useRef, useState} from 'react';
+import {Platform} from 'react-native';
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: true,
+		shouldSetBadge: true,
+	}),
+});
 
 const theme = createTheme({
 	lightColors: {
@@ -14,6 +27,40 @@ const theme = createTheme({
 });
 
 export default function App() {
+	const [expoPushToken, setExpoPushToken] = useState('');
+	const [notification, setNotification] =
+		useState<Notifications.Notification>();
+	const notificationListener = useRef<Subscription>();
+	const responseListener = useRef<Subscription>();
+
+	console.log('notification', notification, expoPushToken);
+
+	useEffect(() => {
+		registerForPushNotificationsAsync().then(
+			token => token && setExpoPushToken(token),
+		);
+
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener(notification => {
+				notification && setNotification(notification);
+			});
+
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener(response => {
+				console.log(response);
+			});
+
+		return () => {
+			notificationListener.current &&
+				Notifications.removeNotificationSubscription(
+					notificationListener.current,
+				);
+
+			responseListener.current &&
+				Notifications.removeNotificationSubscription(responseListener.current);
+		};
+	}, []);
+
 	return (
 		<SafeAreaProvider>
 			<ThemeProvider theme={theme}>
@@ -23,4 +70,47 @@ export default function App() {
 			</ThemeProvider>
 		</SafeAreaProvider>
 	);
+}
+
+export const schedulePushNotification = async (title: string, body: string) => {
+	await Notifications.scheduleNotificationAsync({
+		content: {
+			title,
+			body,
+			data: {data: 'goes here'},
+		},
+		trigger: {seconds: 1},
+	});
+};
+
+async function registerForPushNotificationsAsync() {
+	let token;
+
+	if (Platform.OS === 'android') {
+		await Notifications.setNotificationChannelAsync('default', {
+			name: 'default',
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: '#FF231F7C',
+		});
+	}
+
+	if (Device.isDevice) {
+		const {status: existingStatus} = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== 'granted') {
+			const {status} = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== 'granted') {
+			alert('Failed to get push token for push notification!');
+			return;
+		}
+		token = (await Notifications.getExpoPushTokenAsync()).data;
+		console.log(token);
+	} else {
+		alert('Must use physical device for Push Notifications');
+	}
+
+	return token;
 }
--- app/src/screens/home.tsx
+++ app/src/screens/home.tsx
@@ -2,12 +2,50 @@ import {View, Text} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {RootStackParamList} from '../routes/stack';
 import {StackScreenProps} from '@react-navigation/stack';
-import {Book} from '../../../db/prisma';
+import {Book, Section} from '../../../db/prisma';
+import {Button} from '@rneui/base';
+import {schedulePushNotification} from '../../App';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Props = StackScreenProps<RootStackParamList, 'Home'>;
 
+type FullBook = Book & {sections: Section[]};
+
+type LocalData = {
+	book: FullBook;
+	currentSection: number;
+};
+
 export const HomeScreen = ({navigation}: Props) => {
 	const [books, setBooks] = useState([]);
+	const [currentData, setCurrentData] = useState<LocalData>();
+
+	const storeData = async (value: LocalData) => {
+		try {
+			const jsonValue = JSON.stringify(value);
+			await AsyncStorage.setItem('@local_data', jsonValue);
+			setCurrentData(value);
+		} catch (e) {
+			// saving error
+		}
+	};
+
+	const getData = async () => {
+		try {
+			const jsonValue = await AsyncStorage.getItem('@local_data');
+			return jsonValue != null ? JSON.parse(jsonValue) : null;
+		} catch (e) {
+			// error reading value
+		}
+	};
+
+	useEffect(() => {
+		const setData = async () => {
+			const data = await getData();
+			setCurrentData(() => data);
+		};
+		setData();
+	}, []);
 
 	useEffect(() => {
 		const getBooks = async () =>
@@ -15,19 +53,57 @@ export const HomeScreen = ({navigation}: Props) => {
 		getBooks()
 			.then(res => res.json())
 			.then(res => setBooks(res));
-	});
+	}, []);
+
+	const onClickRead = (book: FullBook) => {
+		storeData({book, currentSection: 0});
+	};
 
 	return (
 		<View className='h-full w-full px-2'>
-			<Text className='text-5xl font-bold mx-auto pb-10 '>
-				Livres <Text className='text-indigo-500 text-6xl'>disponibles</Text>
+			<Text className='text-3xl pt-4 font-bold mx-auto pb-10 '>
+				Livres <Text className='text-indigo-500 '>disponibles</Text>
 			</Text>
-			{books.map((book: Book) => (
-				<View className='flex gap-2'>
-					<Text className='text-indigo-500 text-xl'>{book.title}</Text>
-					<Text className='text-indigo-500 text-xl'>{book.}</Text>
+			{books.map((book: FullBook) => (
+				<View className='flex flex-row gap-2 justify-between'>
+					<View className='flex gap-1'>
+						<Text className='text-gray-700 text-xl'>📚 {book.title}</Text>
+						<Text className='text-gray-400 text-lg'>
+							Chapitres : {book.sections.length}
+						</Text>
+						<Text className='text-gray-400 text-lg'>
+							Auteur : {book.author}
+						</Text>
+					</View>
+					<View className='flex items-center justify-center'>
+						<Button onPress={() => onClickRead(book)}>Lire</Button>
+					</View>
 				</View>
 			))}
+			{/* <Button
+				onPress={async () => {
+					console.log('cliack');
+					await schedulePushNotification('Test', 'Body');
+				}}
+			>
+				Send notification
+			</Button>
+			<Button
+				onPress={async () => {
+					await storeData(data);
+				}}
+			>
+				Store Data
+			</Button> */}
+			<Text className='text-3xl pt-4 font-bold mx-auto pb-10 '>
+				Livres <Text className='text-indigo-500 '>en cour</Text>
+			</Text>
+			<Text className='text-indigo-500 text-xl'>
+				{currentData?.book?.title}
+			</Text>
+			<Text className='text-indigo-500 text-xl'>
+				chapitre actuel : {currentData?.currentSection}
+			</Text>
 		</View>
 	);
 };
