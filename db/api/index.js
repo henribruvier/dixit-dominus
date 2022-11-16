6600b6a80131feb4e79423d07e4b41fc230f9643
henribruvier <henri.bruvier@gmail.com>
2022-11-16 12:47:19 +0100

    fix: :bug: new try

--- db/api/index.js
+++ db/api/index.js
@@ -1,17 +1,15 @@
 const app = require("express")();
-const { v4 } = require("uuid");
 const prisma = require("../client");
 
 app.use(express.json());
 
 app.get("/api", async (req, res) => {
-  res.end("Hello World");
+  res.json("Hello World");
 });
 
 app.get("/api/books", async (req, res) => {
   try {
     const books = await prisma.book.findMany();
-
     res.json(books);
   } catch (error) {
     res.status(500).json({
