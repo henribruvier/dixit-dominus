e68d29dbf1d19c1a9144cd048a064d2a0c35627c
pnxdxt <paul.nodet@gmail.com>
2022-11-25 23:02:44 +0100

    feat: :lipstick: clean ui

---
+++ app/src/data/settings.ts
@@ -0,0 +1,15 @@
+export const delayParser = {
+	'20 min': 20 * 60,
+	'40 min': 40 * 60,
+	'1h': 60 * 60,
+	'1h30': 90 * 60,
+	'10 sec': 10,
+};
+
+export const settingsData = [
+	{key: '1', value: '20 min'},
+	{key: '2', value: '40 min'},
+	{key: '3', value: '1h'},
+	{key: '4', value: '1h30'},
+	{key: '6', value: '10 sec'},
+];
